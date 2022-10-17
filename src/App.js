434a281a4b626b9104a4d45b318c8182c047a376
Khurshid <axurshidbek2005@gmail.com>
2022-10-17 18:47:18 +0500

    first React web layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+
 import './App.css';
+// components
+import Colons from './components/colon';
+// sources
+import Logo from './img/book_logo.png';
 
 function App() {
   return (
     <div className="App">
+      {/* header */}
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={Logo} alt="" />
+        <a href='#!'> books.uz</a>
       </header>
+      {/* hero */}
+      <section className="App-hero">
+        <h1>We read a wonder books together...</h1>
+      </section>
+      <section className='App-colons'>
+        <Colons />
+        <Colons />
+        <Colons />
+      </section>
+      {/* footer */}
+      <footer>
+        <div className="footer">
+          <img src={Logo} alt="" />
+          <div className="contacts">
+            <h4>Единый кол-центр (круглосуточно)</h4>
+            <b>+998 93 857 77 66</b>
+          </div>
+        </div>
+      </footer>
+
     </div>
   );
 }
---
+++ src/components/colon.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../App.css'
+
+export default function colon() {
+    return (
+        <div className='colons'>
+            <h4>Why you are read books?</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non. </p>
+        </div>
+    )
+}
